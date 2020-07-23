0bdab4be19c3b3f9f816de1db95a8e20b0aeb263
David <davidchall388@gmail.com>
2020-07-23 17:44:01 -0400

    small css style tweaks, added opacity change for potential trash

--- app.js
+++ app.js
@@ -14,10 +14,8 @@ $(() => {
         if (!$(event.target).hasClass("control")){
             return;
         } else if($(event.target).hasClass("copy")) {
-            console.log("insecond if")
             $activeControl=$(event.target);
             activePosition=$activeControl.position();
-            console.log(`x ${activePosition.left}   y${activePosition.top}`)
         } else {
             $activeControl=$(event.target).clone().addClass("copy").appendTo(".container");
             $activeControl.css({top: lastY, left: lastX, position: 'absolute'})
@@ -26,12 +24,18 @@ $(() => {
         mouseOffsetX=event.pageX-activePosition.left;
         mouseOffsetY=event.pageY-activePosition.top;
         $document.mousemove((event)=>{
-            console.log(mouseOffsetX)
             deltaX=lastX-event.pageX;
             deltaY=lastY-event.pageY;
             $activeControl.css({top: lastY+deltaY-mouseOffsetY, left: lastX+deltaX-mouseOffsetX})
             lastX=event.pageX;
             lastY=event.pageY;
+            let trashPos=$(".trash").position();
+            if((event.pageX>trashPos.left && event.pageX<trashPos.left+100) && 
+            event.pageY>trashPos.top && event.pageY<trashPos.top+100) {
+            $(event.target).addClass("possibleTrash")
+            } else {
+                $(event.target).removeClass("possibleTrash")
+            }
         })
     })
 
@@ -40,8 +44,7 @@ $(() => {
         if((event.pageX>trashPos.left && event.pageX<trashPos.left+100) && 
             event.pageY>trashPos.top && event.pageY<trashPos.top+100) {
             $(event.target).remove();
-        }
-        console.log("in mouseup")
+        } 
         $document.off("mousemove");
     })
 
@@ -51,26 +54,9 @@ $(() => {
         let $controllerPos=$(".controller").position();
         $.each($components, (index,element) => {
             let $element=$(element)
-            result.push({xPos:$element.position().left-$controllerPos.left, yPos:$element.position().top-$controllerPos.top, id:$element.attr('id')})
+            result.push({xPos:$element.position().left-$controllerPos.left+37.5, yPos:$element.position().top-$controllerPos.top+37.5, id:$element.attr('id')})
 
         })
-            // console.log(element);
-            // console.log($element)
             console.log(result)
         };
-        // console.log($(result[1]).position());
-        // console.log($(result[1]).attr('id'));
-
-    
-    
-    // $joyStick1.mousedown(()=>{
-    //     $activeControl=$joyStick1.clone()
-    // });
-
-    // $joyStick1.mouseup(()=>{
-    //     $activeControl=null;
-    // })
-
-
-
 });
