d046adab4b806e64808e7b2cfbd16a155603f95e
David <davidchall388@gmail.com>
2020-09-23 10:43:39 -0400

    pushing old commit

--- app.js
+++ app.js
@@ -8,25 +8,25 @@ $(() => {
     let buttonData=[];
     let $controllerPos=$(".controller").position();
 
-    let $axis1=$("#axis1"), $axis2=$("#axis2")
+    let $axis1=$("#axis1"), $axis2=$("#axis2"), $axis4=$("#axis4");
 
     $axis1.click(() => {
-        $menu.offset({top:20+$controllerPos.top,left:20+$controllerPos.left});
-        $psXbox.offset({top:20+$controllerPos.top,left:70+$controllerPos.left});
-        $view.offset({top:20+$controllerPos.top,left:120+$controllerPos.left});
-        $digitalJoy.offset({top:20+$controllerPos.top,left:300+$controllerPos.left});
-        $analogJoy1.offset({top:280+$controllerPos.top,left:20+$controllerPos.left});
-        $analogJoy2.offset({top:280+$controllerPos.top,left:580+$controllerPos.left});
-        $l3.offset({top:130+$controllerPos.top,left:20+$controllerPos.left});
-        $r3.offset({top:130+$controllerPos.top,left:605+$controllerPos.left});
-        $rt.offset({top:170+$controllerPos.top,left:470+$controllerPos.left});
-        $rb.offset({top:265+$controllerPos.top,left:470+$controllerPos.left});
-        $y.offset({top:170+$controllerPos.top,left:370+$controllerPos.left});
-        $b.offset({top:265+$controllerPos.top,left:370+$controllerPos.left});
-        $x.offset({top:170+$controllerPos.top,left:270+$controllerPos.left});
-        $a.offset({top:265+$controllerPos.top,left:270+$controllerPos.left});
-        $lt.offset({top:170+$controllerPos.top,left:170+$controllerPos.left});
-        $lb.offset({top:265+$controllerPos.top,left:170+$controllerPos.left});
+        $menu.css({position: 'absolute'}).offset({top:20+$controllerPos.top,left:20+$controllerPos.left});
+        $psXbox.css({position: 'absolute'}).offset({top:20+$controllerPos.top,left:70+$controllerPos.left});
+        $view.css({position: 'absolute'}).offset({top:20+$controllerPos.top,left:120+$controllerPos.left});
+        $digitalJoy.css({position: 'absolute'}).offset({top:20+$controllerPos.top,left:300+$controllerPos.left});
+        $analogJoy1.css({position: 'absolute'}).offset({top:280+$controllerPos.top,left:20+$controllerPos.left});
+        $analogJoy2.css({position: 'absolute'}).offset({top:280+$controllerPos.top,left:580+$controllerPos.left});
+        $l3.css({position: 'absolute'}).offset({top:130+$controllerPos.top,left:20+$controllerPos.left});
+        $r3.css({position: 'absolute'}).offset({top:130+$controllerPos.top,left:605+$controllerPos.left});
+        $rt.css({position: 'absolute'}).offset({top:170+$controllerPos.top,left:470+$controllerPos.left});
+        $rb.css({position: 'absolute'}).offset({top:265+$controllerPos.top,left:470+$controllerPos.left});
+        $y.css({position: 'absolute'}).offset({top:170+$controllerPos.top,left:370+$controllerPos.left});
+        $b.css({position: 'absolute'}).offset({top:265+$controllerPos.top,left:370+$controllerPos.left});
+        $x.css({position: 'absolute'}).offset({top:170+$controllerPos.top,left:270+$controllerPos.left});
+        $a.css({position: 'absolute'}).offset({top:265+$controllerPos.top,left:270+$controllerPos.left});
+        $lt.css({position: 'absolute'}).offset({top:170+$controllerPos.top,left:170+$controllerPos.left});
+        $lb.css({position: 'absolute'}).offset({top:265+$controllerPos.top,left:170+$controllerPos.left});
     })
 
     $axis2.click(() => {
@@ -48,39 +48,57 @@ $(() => {
         $lb.offset({top:180+$controllerPos.top,left:120+$controllerPos.left});
     })
 
+    $axis4.click(() => {
+        $menu.offset({top:20+$controllerPos.top,left:270+$controllerPos.left});
+        $psXbox.offset({top:100+$controllerPos.top,left:270+$controllerPos.left});
+        $view.offset({top:180+$controllerPos.top,left:270+$controllerPos.left});
+        $digitalJoy.offset({top:30+$controllerPos.top,left:75+$controllerPos.left});
+        $analogJoy1.offset({top:280+$controllerPos.top,left:20+$controllerPos.left});
+        $analogJoy2.offset({top:280+$controllerPos.top,left:225+$controllerPos.left});
+        $l3.offset({top:200+$controllerPos.top,left:400+$controllerPos.left});
+        $r3.offset({top:200+$controllerPos.top,left:485+$controllerPos.left});
+        $rt.offset({top:30+$controllerPos.top,left:605+$controllerPos.left});
+        $rb.offset({top:120+$controllerPos.top,left:605+$controllerPos.left});
+        $y.offset({top:30+$controllerPos.top,left:520+$controllerPos.left});
+        $b.offset({top:120+$controllerPos.top,left:520+$controllerPos.left});
+        $x.offset({top:30+$controllerPos.top,left:435+$controllerPos.left});
+        $a.offset({top:120+$controllerPos.top,left:435+$controllerPos.left});
+        $lt.offset({top:30+$controllerPos.top,left:350+$controllerPos.left});
+        $lb.offset({top:120+$controllerPos.top,left:350+$controllerPos.left});
+    })
+
     $document.mousedown((event)=>{
+        console.log(event.target)
+
+        if (!$(event.target).hasClass("control")){
+            console.log("in first if")
+            return;
+        }
 
         let deltaX, deltaY, lastX=event.pageX, lastY=event.pageY, activePosition, mouseOffsetX, mouseOffsetY;
-        if (!$(event.target).hasClass("control")){
-            return;
-        } else if($(event.target).hasClass("copy")) {
             $activeControl=$(event.target);
             activePosition=$activeControl.position();
-        } else {
-            $activeControl=$(event.target).clone().addClass("copy").appendTo(".container");
-            $activeControl.css({top: lastY, left: lastX, position: 'absolute'})
-            activePosition=$activeControl.position();
-        }
+            console.log(activePosition.left)
+            $activeControl.css({position: 'absolute'})
         mouseOffsetX=event.pageX-activePosition.left;
         mouseOffsetY=event.pageY-activePosition.top;
         $document.mousemove((event)=>{
+            console.log($activeControl)
             deltaX=lastX-event.pageX;
             deltaY=lastY-event.pageY;
             $activeControl.css({top: lastY+deltaY-mouseOffsetY, left: lastX+deltaX-mouseOffsetX})
             lastX=event.pageX;
             lastY=event.pageY;
-            let trashPos=$(".trash").position();
-            if((event.pageX>trashPos.left && event.pageX<trashPos.left+100) && 
-            event.pageY>trashPos.top && event.pageY<trashPos.top+100) {
-            $(event.target).addClass("possibleTrash")
-            } else {
-                $(event.target).removeClass("possibleTrash")
-            }
+            
         })
     })
 
+    $document.mouseup((event)=> {
+        $document.off("mousemove");
+    })
+
     buildArray = () => {
-        let $components=$(".copy")
+        let $components=$(".control")
         let result= [];
         let $controllerPos=$(".controller").position();
         $.each($components, (index,element) => {
