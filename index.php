cbe97e5cdd716b10d53979f9bf38e678cfd1bb51
David <davidchall388@gmail.com>
2020-09-24 10:29:02 -0400

    updated a bunch packages...

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
-<?php include_once("home.html"); 
-
-
-if($_SERVER["HTTPS"] != "on")
-{
-    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-    exit();
-}
+<?php include_once("home.html");
 ?>
