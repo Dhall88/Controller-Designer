bb229492d5e66a8cdcd19d694ad2f26b5fba63ea
David <davidchall388@gmail.com>
2020-09-24 10:02:13 -0400

    the saga continues

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php include_once("home.html"); ?>
+<?php include_once("home.html"); 
+
+
+if($_SERVER["HTTPS"] != "on")
+{
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    exit();
+}
+?>
