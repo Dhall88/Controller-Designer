6ad507c849e1ee0d24fe803ced99a3b5c5447b41
David <davidchall388@gmail.com>
2020-09-25 18:09:25 -0400

    pushing alert ot heroku

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include_once("home.html");
 if($_POST){
-    
+    echo "<script type='text/javascript'>alert('SUCESS IN PHP');</script>";
 
 //send email
     mail("dhall87@gmail.com", "This is a test", "test test test");
