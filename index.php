95390f559ef8158f39c6b5ffaa49963f14b142fb
David <davidchall388@gmail.com>
2020-09-25 18:21:19 -0400

    one more try to heroku

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php include_once("home.html");
 if($_POST){
+
+    echo '<script language="javascript">';
+echo 'alert("message successfully sent")';
+echo '</script>';
     
     mail("dhall87@gmail.com", "This is a test", "test test test");
 }
