f1c12d6df4cd9d93f950d34af3e327cc105559b4
David <davidchall388@gmail.com>
2020-09-25 19:54:36 -0400

    testing git

--- email.php
+++ email.php
@@ -2,31 +2,24 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require_once "vendor/autoload.php";
+$mail = new PHPMailer();
 
-//PHPMailer Object
-$mail = new PHPMailer(true); //Argument true in constructor enables exceptions
+// Settings
+$mail->IsSMTP();
+$mail->CharSet = 'UTF-8';
 
-//From email address and name
-$mail->From = "dhall87@gmail.com";
-$mail->FromName = "David";
+$mail->Host       = "smtp.gmail.com"; // SMTP server example
+$mail->SMTPDebug  = 0;                     // enables SMTP debug information (for testing)
+$mail->SMTPAuth   = true;                  // enable SMTP authentication
+$mail->Port       = 465;                    // set the SMTP port for the GMAIL server
+$mail->Username   = "username"; // SMTP account username example
+$mail->Password   = "password";        // SMTP account password example
 
-//To address and name
-$mail->addAddress("dhall87@gmail.com");
-
-// //Address to which recipient will reply
-// $mail->addReplyTo("reply@yourdomain.com", "Reply");
-
-// //CC and BCC
-// $mail->addCC("cc@example.com");
-// $mail->addBCC("bcc@example.com");
-
-//Send HTML or Plain Text email
-$mail->isHTML(true);
-
-$mail->Subject = "TEST";
-$mail->Body = "<i>TESTESTEST</i>";
-$mail->AltBody = "This is the plain text version of the email content";
+// Content
+$mail->isHTML(true);                                  // Set email format to HTML
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 try {
     $mail->send();
@@ -34,4 +27,41 @@ try {
 } catch (Exception $e) {
     echo "Mailer Error: " . $mail->ErrorInfo;
 }
+
+
+
 ?>
+
+
+
+// //PHPMailer Object
+// $mail = new PHPMailer(true); //Argument true in constructor enables exceptions
+
+// //From email address and name
+// $mail->From = "dhall87@gmail.com";
+// $mail->FromName = "David";
+
+// //To address and name
+// $mail->addAddress("dhall87@gmail.com");
+
+// // //Address to which recipient will reply
+// // $mail->addReplyTo("reply@yourdomain.com", "Reply");
+
+// // //CC and BCC
+// // $mail->addCC("cc@example.com");
+// // $mail->addBCC("bcc@example.com");
+
+// //Send HTML or Plain Text email
+// $mail->isHTML(true);
+
+// $mail->Subject = "TEST";
+// $mail->Body = "<i>TESTESTEST</i>";
+// $mail->AltBody = "This is the plain text version of the email content";
+
+// try {
+//     $mail->send();
+//     echo "Message has been sent successfully";
+// } catch (Exception $e) {
+//     echo "Mailer Error: " . $mail->ErrorInfo;
+// }
+// ?>
