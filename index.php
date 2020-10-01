78be45f0560bcb173a577e896a34a2a861fd5d22
David <davidchall388@gmail.com>
2020-10-01 11:23:50 -0400

    think I have heroku sendgrid fix

--- index.php
+++ index.php
@@ -10,16 +10,16 @@ if($_POST){
 
 echo "IN PHP";
 $email = new \SendGrid\Mail\Mail();
-$email->setFrom("SENDER_EMAIL", "Example User");
+$email->setFrom("controllercoords@gmail.com", "Example User");
 $email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("RECIEVER_EMAIL", "Example User");
+$email->addTo("dhall87@gmail.com", "Example User");
 $email->addContent(
     "text/plain", "and easy to do anywhere, even with PHP"
 );
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
-$sendgrid = new \SendGrid('SENDGRID_API_KEY');
+$sendgrid = new \SendGrid('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');
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
