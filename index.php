82a7ae4b81ce82b429de9b09ac0a76a01b17a964
David <davidchall388@gmail.com>
2020-10-02 14:24:49 -0400

    heroku error for vendor folder dealt with

--- index.php
+++ index.php
@@ -4,25 +4,25 @@ include_once("home.html");
 
 require './vendor/autoload.php';
 
-// if($_POST){
+if($_POST){
 
-    // $digitalJoy=$_POST['digitalJoy'];
-    // $analogJoy1=$_POST['analogJoy1'];
-    // $analogJoy1=$_POST['analogJoy2'];
-    // $digitalJoy=$_POST['digitalJoy'];
-    // $rt=$_POST['rt'];
-    // $rb=$_POST['rb'];
-    // $r3=$_POST['r3'];
-    // $lt=$_POST['lt'];
-    // $lb=$_POST['lb'];
-    // $l3=$_POST['l3'];
-    // $x=$_POST['x'];
-    // $y=$_POST['y'];
-    // $a=$_POST['b'];
-    // $b=$_POST['b'];
-    // $psXbox=$_POST['psXbox'];
-    // $menu=$_POST['menu'];
-    // $view=$_POST['view'];
+    $digitalJoy=$_POST['digitalJoy'];
+    $analogJoy1=$_POST['analogJoy1'];
+    $analogJoy1=$_POST['analogJoy2'];
+    $digitalJoy=$_POST['digitalJoy'];
+    $rt=$_POST['rt'];
+    $rb=$_POST['rb'];
+    $r3=$_POST['r3'];
+    $lt=$_POST['lt'];
+    $lb=$_POST['lb'];
+    $l3=$_POST['l3'];
+    $x=$_POST['x'];
+    $y=$_POST['y'];
+    $a=$_POST['b'];
+    $b=$_POST['b'];
+    $psXbox=$_POST['psXbox'];
+    $menu=$_POST['menu'];
+    $view=$_POST['view'];
 
     // echo "IN PHP";
     print_r($_POST);
@@ -35,22 +35,22 @@ require './vendor/autoload.php';
     );
     $email->addContent(
         "text/html", "testing"
-        // "Digital Joystick: x=$digitalJoy[0] y=$digitalJoy[1] <br>
-        // Analog Joystick 1: x=$analogJoy1[0] y=$analogJoy1[1] <br>
-        // Analog Joystick 2: x=$analogJoy2[0] y=$analogJoy2[1] <br>
-        // RT: x=$rt[0] y=$rt[1] <br>
-        // RB: x=$rb[0] y=$rb[1] <br>
-        // R3: x=$r3[0] y=$r3[1] <br>
-        // LT: x=$lt[0] y=$lt[1] <br>
-        // LB: x=$lb[0] y=$lb[1] <br>
-        // L3: x=$l3[0] y=$l3[1] <br>
-        // X: x=$x[0] y=$x[1] <br>
-        // Y: x=$y[0] y=$y[1] <br>
-        // A: x=$a[0] y=$a[1] <br>
-        // B: x=$b[0] y=$b[1] <br>
-        // ps/xbox: x=$psXbox[0] y=$psXbox[1] <br>
-        // menu: x=$menu[0] y=$menu[1] <br>
-        // view: x=$view[0] y=$view[1] <br>"
+        "Digital Joystick: x=$digitalJoy[0] y=$digitalJoy[1] <br>
+        Analog Joystick 1: x=$analogJoy1[0] y=$analogJoy1[1] <br>
+        Analog Joystick 2: x=$analogJoy2[0] y=$analogJoy2[1] <br>
+        RT: x=$rt[0] y=$rt[1] <br>
+        RB: x=$rb[0] y=$rb[1] <br>
+        R3: x=$r3[0] y=$r3[1] <br>
+        LT: x=$lt[0] y=$lt[1] <br>
+        LB: x=$lb[0] y=$lb[1] <br>
+        L3: x=$l3[0] y=$l3[1] <br>
+        X: x=$x[0] y=$x[1] <br>
+        Y: x=$y[0] y=$y[1] <br>
+        A: x=$a[0] y=$a[1] <br>
+        B: x=$b[0] y=$b[1] <br>
+        ps/xbox: x=$psXbox[0] y=$psXbox[1] <br>
+        menu: x=$menu[0] y=$menu[1] <br>
+        view: x=$view[0] y=$view[1] <br>"
     );
     $sendgrid = new \SendGrid(getenv("SENDGRID_API_KEY"));
     try {
@@ -61,5 +61,5 @@ require './vendor/autoload.php';
     } catch (Exception $e) {
         echo 'Caught exception: ',  $e->getMessage(), "\n";
     }
-// }
+}
 ?>
