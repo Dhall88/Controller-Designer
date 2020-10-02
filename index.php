3bb4f0c3a1e4cfafb4d84e8a8e4dc02e28b5fab0
David <davidchall388@gmail.com>
2020-10-02 14:20:20 -0400

    heroku sendgrid error....again

--- index.php
+++ index.php
@@ -4,54 +4,55 @@ include_once("home.html");
 
 require './vendor/autoload.php';
 
-if($_POST){
+// if($_POST){
 
-    $digitalJoy=$_POST['digitalJoy'];
-    $analogJoy1=$_POST['analogJoy1'];
-    $analogJoy1=$_POST['analogJoy2'];
-    $digitalJoy=$_POST['digitalJoy'];
-    $rt=$_POST['rt'];
-    $rb=$_POST['rb'];
-    $r3=$_POST['r3'];
-    $lt=$_POST['lt'];
-    $lb=$_POST['lb'];
-    $l3=$_POST['l3'];
-    $x=$_POST['x'];
-    $y=$_POST['y'];
-    $a=$_POST['b'];
-    $b=$_POST['b'];
-    $psXbox=$_POST['psXbox'];
-    $menu=$_POST['menu'];
-    $view=$_POST['view'];
+    // $digitalJoy=$_POST['digitalJoy'];
+    // $analogJoy1=$_POST['analogJoy1'];
+    // $analogJoy1=$_POST['analogJoy2'];
+    // $digitalJoy=$_POST['digitalJoy'];
+    // $rt=$_POST['rt'];
+    // $rb=$_POST['rb'];
+    // $r3=$_POST['r3'];
+    // $lt=$_POST['lt'];
+    // $lb=$_POST['lb'];
+    // $l3=$_POST['l3'];
+    // $x=$_POST['x'];
+    // $y=$_POST['y'];
+    // $a=$_POST['b'];
+    // $b=$_POST['b'];
+    // $psXbox=$_POST['psXbox'];
+    // $menu=$_POST['menu'];
+    // $view=$_POST['view'];
 
     // echo "IN PHP";
     print_r($_POST);
     $email = new \SendGrid\Mail\Mail();
-    $email->setFrom(getenv('SENDER_EMAIL'), "Example User");
+    $email->setFrom(getenv("SENDER_EMAIL"), "Example User");
     $email->setSubject("Sending with SendGrid is Fun");
-    $email->addTo(getenv('REVIEVER_EMAIL'), "Example User");
+    $email->addTo(getenv("RECIEVER_EMAIL"), "Example User");
     $email->addContent(
         "text/plain", 'test'
     );
     $email->addContent(
-        "text/html", "Digital Joystick: x=$digitalJoy[0] y=$digitalJoy[1] <br>
-        Analog Joystick 1: x=$analogJoy1[0] y=$analogJoy1[1] <br>
-        Analog Joystick 2: x=$analogJoy2[0] y=$analogJoy2[1] <br>
-        RT: x=$rt[0] y=$rt[1] <br>
-        RB: x=$rb[0] y=$rb[1] <br>
-        R3: x=$r3[0] y=$r3[1] <br>
-        LT: x=$lt[0] y=$lt[1] <br>
-        LB: x=$lb[0] y=$lb[1] <br>
-        L3: x=$l3[0] y=$l3[1] <br>
-        X: x=$x[0] y=$x[1] <br>
-        Y: x=$y[0] y=$y[1] <br>
-        A: x=$a[0] y=$a[1] <br>
-        B: x=$b[0] y=$b[1] <br>
-        ps/xbox: x=$psXbox[0] y=$psXbox[1] <br>
-        menu: x=$menu[0] y=$menu[1] <br>
-        view: x=$view[0] y=$view[1] <br>"
+        "text/html", "testing"
+        // "Digital Joystick: x=$digitalJoy[0] y=$digitalJoy[1] <br>
+        // Analog Joystick 1: x=$analogJoy1[0] y=$analogJoy1[1] <br>
+        // Analog Joystick 2: x=$analogJoy2[0] y=$analogJoy2[1] <br>
+        // RT: x=$rt[0] y=$rt[1] <br>
+        // RB: x=$rb[0] y=$rb[1] <br>
+        // R3: x=$r3[0] y=$r3[1] <br>
+        // LT: x=$lt[0] y=$lt[1] <br>
+        // LB: x=$lb[0] y=$lb[1] <br>
+        // L3: x=$l3[0] y=$l3[1] <br>
+        // X: x=$x[0] y=$x[1] <br>
+        // Y: x=$y[0] y=$y[1] <br>
+        // A: x=$a[0] y=$a[1] <br>
+        // B: x=$b[0] y=$b[1] <br>
+        // ps/xbox: x=$psXbox[0] y=$psXbox[1] <br>
+        // menu: x=$menu[0] y=$menu[1] <br>
+        // view: x=$view[0] y=$view[1] <br>"
     );
-    $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+    $sendgrid = new \SendGrid(getenv("SENDGRID_API_KEY"));
     try {
         $response = $sendgrid->send($email);
         print $response->statusCode() . "\n";
@@ -60,5 +61,5 @@ if($_POST){
     } catch (Exception $e) {
         echo 'Caught exception: ',  $e->getMessage(), "\n";
     }
-}
+// }
 ?>
