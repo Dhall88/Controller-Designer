568ef0047564ae5a509905ad8980c4265c113b4c
David Hall <davidchall388@gmail.com>
2021-03-28 14:06:42 -0400

    removed old email body as its not nessesary

--- index.php
+++ index.php
@@ -121,22 +121,7 @@ $email->addAttachment(
     $email->addContent(
         "text/html", "Name: $name  <br>
         Contact Email: $contactEmail <br>
-        Digital Joystick: x=$digitalJoy[0] y=$digitalJoy[1] <br>
-        Analog Joystick 1: x=$analogJoy1[0] y=$analogJoy1[1] <br>
-        Analog Joystick 2: x=$analogJoy2[0] y=$analogJoy2[1] <br>
-        RT: x=$rt[0] y=$rt[1] <br>
-        RB: x=$rb[0] y=$rb[1] <br>
-        R3: x=$r3[0] y=$r3[1] <br>
-        LT: x=$lt[0] y=$lt[1] <br>
-        LB: x=$lb[0] y=$lb[1] <br>
-        L3: x=$l3[0] y=$l3[1] <br>
-        Square/X: x=$x[0] y=$x[1] <br>
-        Triangle/Y: x=$y[0] y=$y[1] <br>
-        X/A: x=$a[0] y=$a[1] <br>
-        Cricle/B: x=$b[0] y=$b[1] <br>
-        ps/xbox: x=$psXbox[0] y=$psXbox[1] <br>
-        menu: x=$menu[0] y=$menu[1] <br>
-        view: x=$view[0] y=$view[1] <br>"
+        Testing new CAD feature"
     );
     $sendgrid = new \SendGrid(getenv("SENDGRID_API_KEY"));
     try {
